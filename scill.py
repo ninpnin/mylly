0ddff11cbc66192cb67f23e93afffd0aa88c212c
ninpnin <vainoyrjanainen@icloud.com>
2021-08-11 19:58:41 +0200

    Write App.scala by default

--- scill.py
+++ scill.py
@@ -13,6 +13,14 @@ def mill_body(name, scala_version="2.13.4"):
 }""" % (name, scala_version)
     return body
 
+def app_body(name):
+    body = """package %s
+
+object Application extends App {
+    println(\"Hello Beer!\")
+}""" % (name)
+    return body
+
 def main():
     p = Path(".")
     print("Init scala project for mill")
@@ -38,6 +46,12 @@ def main():
     pkg_src.mkdir(parents=True)
     pkg_src_pkg = pkg_src / package_name
     pkg_src_pkg.mkdir(parents=True)
+    pkg_src_pkg_app = pkg_src_pkg / "App.scala"
+    pkg_src_pkg_app.touch()
+    pkg_src_pkg_app = pkg_src_pkg_app.open(mode='w')
+    app_content = app_body(package_name)
+    pkg_src_pkg_app.write(app_content)
+    pkg_src_pkg_app.close()
 
 
 if __name__ == '__main__':
